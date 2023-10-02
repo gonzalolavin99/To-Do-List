e8e84c883e55e4b7b0d591b8aac8aa972e2b77b7
gonzalo lavin <gonzalolavin99@gmail.com>
2023-10-02 01:34:11 -0300

    Creada la función de agregar tareas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,25 @@
 const inputTareas = document.querySelector('#nuevasTareas');
 const btnAgregar = document.querySelector('#agregarTareas');
 const bodyTable = document.querySelector('tbody');
+let ultimoID = 0;
 const tareas = [];
+
+
+function renderTareas (){
+    let html = "";
+    for (let tarea of tareas){
+        html+= `<tr> 
+        <td>${tarea.id}</td>
+        <td>${tarea.nombreTarea}</td>
+        <td><input type="checkbox" id="checkbox" name="checkbox"></td>
+        </>`
+    }
+    bodyTable.innerHTML= html;
+}
+btnAgregar.addEventListener("click", ()=>{
+    const tareaNueva = inputTareas.value;
+    tareas.push({id: ++ultimoID, nombreTarea:tareaNueva});
+    inputTareas.value="";
+    renderTareas();
+})
+ 
