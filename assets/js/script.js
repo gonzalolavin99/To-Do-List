53513c6ca77b1018ea2fcd4498acf82337812ce0
gonzalo lavin <gonzalolavin99@gmail.com>
2023-10-02 01:13:22 -0300

    Creacion de  interfaz y definición de  variables

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+const inputTareas = document.querySelector('#nuevasTareas');
+const btnAgregar = document.querySelector('#agregarTareas');
+const bodyTable = document.querySelector('tbody');
+const tareas = [];
